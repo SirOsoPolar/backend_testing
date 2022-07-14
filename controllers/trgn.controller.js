32cb1129bbb1f0be7f373a3ce5d07cf2cc2eddaa
SirOsoPolar <ogolar@outlook.es>
2022-07-14 08:24:09 -0500

    Mensaje de commit desde visual studio code

--- controllers/trgn.controller.js
+++ controllers/trgn.controller.js
@@ -2,7 +2,7 @@
 // const coseno = x => Math.cos(x)
 // const tan = x => Math.tan(x)
 
-const seno = (req,res) => res.json(Math.sin(parseFloat(req.params.id)))
+const seno = (req,res) => res.json(Math.sin(parseFloat(req.params.id))) 
 const coseno = (req,res) => res.json(Math.cos(parseFloat(req.params.id)))
 const tan = (req,res) => res.json(Math.tan(parseFloat(req.params.id)))
 module.exports = 
